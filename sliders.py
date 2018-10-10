7ea9c0eda5dfa5f007c2eb61b5c99566eda6a1d7
Sophie Green <smgreen02@Sophies-MacBook-Air.local>
2018-10-10 17:11:19 -0400

    my first commit

--- sliders.py
+++ sliders.py
@@ -1,19 +1,19 @@
 ''' Present an interactive function explorer with slider widgets.
-    
-    Scrub the sliders to change the properties of the ``sin`` curve, or
-    type into the title text box to update the title of the plot.
-    
-    Use the ``bokeh serve`` command to run the example by executing:
-    
+
+Scrub the sliders to change the properties of the ``sin`` curve, or
+type into the title text box to update the title of the plot.
+
+Use the ``bokeh serve`` command to run the example by executing:
+
     bokeh serve sliders.py
-    
-    at your command prompt. Then navigate to the URL
-    
+
+at your command prompt. Then navigate to the URL
+
     http://localhost:5006/sliders
-    
-    in your browser.
-    
-    '''
+
+in your browser.
+
+'''
 import numpy as np
 
 from bokeh.io import curdoc
@@ -25,12 +25,12 @@ from bokeh.plotting import figure
 # Set up data
 N = 200
 x = np.linspace(0, 4*np.pi, N)
-y = np.(x)^2
+y = x**2
 source = ColumnDataSource(data=dict(x=x, y=y))
 
 
 # Set up plot
-plot = figure(plot_height=400, plot_width=400, title="my sine wave",
+plot = figure(plot_height=400, plot_width=400, title="parabola",
               tools="crosshair,pan,reset,save,wheel_zoom",
               x_range=[0, 4*np.pi], y_range=[-2.5, 2.5])
 
@@ -38,7 +38,7 @@ plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
 
 
 # Set up widgets
-text = TextInput(title="title", value='my parabola')
+text = TextInput(title="title", value='my sine wave')
 offset = Slider(title="offset", value=0.0, start=-5.0, end=5.0, step=0.1)
 amplitude = Slider(title="amplitude", value=1.0, start=-5.0, end=5.0, step=0.1)
 phase = Slider(title="phase", value=0.0, start=0.0, end=2*np.pi)
@@ -52,17 +52,17 @@ def update_title(attrname, old, new):
 text.on_change('value', update_title)
 
 def update_data(attrname, old, new):
-    
+
     # Get the current slider values
     a = amplitude.value
     b = offset.value
     w = phase.value
     k = freq.value
-    
+
     # Generate the new curve
-    x = np.linspace(0, 4*np.pi, N)
-    y = a*np.sin(k*x + w) + b
-    
+    x = np.linspace(-10, 10, N)
+    y = x**2
+
     source.data = dict(x=x, y=y)
 
 for w in [offset, amplitude, phase, freq]:
@@ -74,4 +74,3 @@ inputs = widgetbox(text, offset, amplitude, phase, freq)
 
 curdoc().add_root(row(inputs, plot, width=800))
 curdoc().title = "Sliders"
-
