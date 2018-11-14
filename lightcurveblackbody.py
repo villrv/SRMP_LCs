5af97e22b5fdec03e623e5c8fab11b54b21c1a70
Sophie Green <smgreen02@Sophies-MacBook-Air.local>
2018-11-14 16:55:44 -0500

    session nov 14

--- lightcurveblackbody.py
+++ lightcurveblackbody.py
@@ -49,8 +49,21 @@ def querry_single_osc(object_name):
     detection = np.where(photometry_limit != 'True')
     return photometry_time, photometry_mag, photometry_sigma, photometry_band, detection
 
-photometry_time, photometry_mag, photometry_sigma, photometry_band, detection = querry_single_osc("SN2006ca")
+def querry_distance(object_name):
+    '''
+    Download a light curve from the Open Supernova Catalog.
+    The output will be in MJD and Magnitude
+    '''
+    osc_link    = 'https://astrocats.space/api/' + object_name + '/lumdist'
+    osc_request = requests.get(osc_link).json()
+    osc_data    = osc_request[object_name]
+    lumdist = np.array(osc_data['lumdist'][0]['value']).astype(float)
 
+    return lumdist
+
+photometry_time, photometry_mag, photometry_sigma, photometry_band, detection = querry_single_osc("DES17C1ffz")
+
+lumdist = querry_distance("DES17C1ffz")
 
 N = 200
 t = np.linspace(0, 100, N)
@@ -77,11 +90,13 @@ plot = figure(plot_height=400, plot_width=400, title="super cool parabola",
               x_range=[0, 100], y_range=[5, 20])
 
 plot.line('x', 'y', source=source, line_width=3, line_alpha=0.6)
-plot.line('x', 'yB', source=source, line_width=3, line_alpha=0.6)
-plot.line('x', 'yr', source=source, line_width=3, line_alpha=0.6)
-plot.line('x', 'yi', source=source, line_width=3, line_alpha=0.6)
-plot.line('x', 'yV', source=source, line_width=3, line_alpha=0.6)
-plot.line('x', 'yU', source=source, line_width=3, line_alpha=0.6)
+plot.line('x', 'yB', source=source, line_width=3, line_alpha=0.6, color="pink")
+plot.line('x', 'yr', source=source, line_width=3, line_alpha=0.6, color="orange")
+plot.line('x', 'yi', source=source, line_width=3, line_alpha=0.6, color="blue")
+plot.line('x', 'yV', source=source, line_width=3, line_alpha=0.6, color="turquoise")
+plot.line('x', 'yU', source=source, line_width=3, line_alpha=0.6, color="purple")
+
+arrayoftimes = np.array(photometry_time)
 
 # Set up widgets
 text = TextInput(title="title", value='my parabola')
@@ -89,22 +104,24 @@ M_slider = Slider(title="Ejecta Mass", value=5, start=1.0, end=10.0, step=0.5)
 f_slider = Slider(title="Fraction of Radio Active Stuff", value=0.0, start=0.0, end=1.0, step=0.1)
 v_slider = Slider(title="Ejecta Velocity", value= 12000, start=5000, end=20000, step=1000)
 k_slider = Slider(title="Kapa", value=0.25, start= 0.1, end=0.5, step=0.05)
+T_slider = Slider(title="Time", value= arrayoftimes.min() - 10, start= arrayoftimes.min() - 10, end=arrayoftimes.max() + 10, step= 10)
 
 count = 0
 for x in photometry_time:
-	x = x - 53860
-	if photometry_band[count] == "r\'":
-		plot.circle(x, photometry_mag[count], size=5, color="green", alpha=0.5)
-	elif photometry_band[count] == "i\'":
-		plot.circle(x, photometry_mag[count], size=5, color="blue", alpha=0.5)
-	elif photometry_band[count] == "V":
-		plot.circle(x, photometry_mag[count], size=5, color="purple", alpha=0.5)
-	elif photometry_band[count] == "U":
+	x = x - T_slider.value
+	if photometry_band[count] == "r":
 		plot.circle(x, photometry_mag[count], size=5, color="orange", alpha=0.5)
+	elif photometry_band[count] == "i":
+		plot.circle(x, photometry_mag[count], size=5, color="blue", alpha=0.5)
+	elif photometry_band[count] == "g":
+		plot.circle(x, photometry_mag[count], size=5, color="turquoise", alpha=0.5)
+	elif photometry_band[count] == "z":
+		plot.circle(x, photometry_mag[count], size=5, color="purple", alpha=0.5)
 	elif photometry_band[count] == "B":
-		plot.circle(x, photometry_mag[count], size=5, color="red", alpha=0.5)
+		plot.circle(x, photometry_mag[count], size=5, color="pink", alpha=0.5)
 	count += 1
 
+print(T_slider.value)
 
 show(plot)
 
@@ -136,6 +153,7 @@ def update_data(attrname, old, new):
     f = f_slider.value
     v = v_slider.value * 1.e5
     k = k_slider.value
+    t = np.linspace(0, 100, N)
     tn = 8.8
     B = 13.8
     c = 3*(10**10)
@@ -154,7 +172,7 @@ def update_data(attrname, old, new):
     wavelength_i = 8.06*10**-5
     wavelength_V = 5.51*10**-5
     wavelength_U = 3.65*10**-5
-    distance = 8.115*10**26
+    distance = lumdist * (3*10**24)
     luminosityblackbody = blackbody(radii, temperature, 5.*(10**-5)*np.ones(len(radii))) * wavelength**2 / c / distance**2
     luminosityblackbody_B = blackbody(radii, temperature, 5.*(10**-5)*np.ones(len(radii))) * wavelength_B**2 / c / distance**2
     luminosityblackbody_r = blackbody(radii, temperature, 5.*(10**-5)*np.ones(len(radii))) * wavelength_r**2 / c / distance**2
@@ -172,12 +190,12 @@ def update_data(attrname, old, new):
     magnitude = -2.5*np.log10(L/4e33)+4.3
     source.data = dict(x=t, y= magblackbody ,yB = magblackbody_B, yr = magblackbody_r,yi = magblackbody_i, yV = magblackbody_V, yU = magblackbody_U)
 
-for w in [M_slider,f_slider,v_slider, k_slider]:
+for w in [M_slider,f_slider,v_slider, k_slider, T_slider]:
     w.on_change('value', update_data)
 
 
 # Set up layouts and add to document
-inputs = widgetbox(text, M_slider, f_slider, v_slider, k_slider)
+inputs = widgetbox(text, M_slider, f_slider, v_slider, k_slider, T_slider)
 
 curdoc().add_root(row(inputs, plot, width=800))
 curdoc().title = "Sliders"
